10e964d12ba3835797d1853c709991efa130cae9
Nino <1005963022@qq.com>
2023-11-02 10:55:18 +0800

    加入了PCB板的IO分布

---
+++ cmake-build-debug-mingw_stm32/CMakeFiles/FOC_OpenLoop.elf.dir/DependInfo.cmake
@@ -0,0 +1,90 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/Clion_for_STM32/FOC_OpenLoop/Core/Startup/startup_stm32h743vitx.s" "E:/Clion_for_STM32/FOC_OpenLoop/cmake-build-debug-mingw_stm32/CMakeFiles/FOC_OpenLoop.elf.dir/Core/Startup/startup_stm32h743vitx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32H743xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "E:/Clion_for_STM32/FOC_OpenLoop/Core/Inc"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Inc"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Inc/Legacy"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/include"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/CMSIS/Device/ST/STM32H7xx/Include"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/CMSIS/Include"
+  "E:/Clion_for_STM32/FOC_OpenLoop/UserApp/retarget"
+  "E:/Clion_for_STM32/FOC_OpenLoop/UserApp/FOC"
+  "E:/Clion_for_STM32/FOC_OpenLoop/UserApp/MT6701"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/Clion_for_STM32/FOC_OpenLoop/Core/Src/freertos.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/freertos.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/freertos.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Core/Src/main.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/main.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Core/Src/stm32h7xx_hal_msp.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/stm32h7xx_hal_msp.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/stm32h7xx_hal_msp.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Core/Src/stm32h7xx_it.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/stm32h7xx_it.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/stm32h7xx_it.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Core/Src/syscalls.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/syscalls.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Core/Src/sysmem.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/sysmem.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Core/Src/system_stm32h7xx.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/system_stm32h7xx.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Core/Src/system_stm32h7xx.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_spi.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_spi.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_spi.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_spi_ex.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_spi_ex.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_spi_ex.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/list.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/UserApp/FOC/FOC.c" "CMakeFiles/FOC_OpenLoop.elf.dir/UserApp/FOC/FOC.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/UserApp/FOC/FOC.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/UserApp/FOC/lowpassFilter.c" "CMakeFiles/FOC_OpenLoop.elf.dir/UserApp/FOC/lowpassFilter.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/UserApp/FOC/lowpassFilter.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/UserApp/FOC/pid.c" "CMakeFiles/FOC_OpenLoop.elf.dir/UserApp/FOC/pid.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/UserApp/FOC/pid.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/UserApp/MT6701/MT6701.c" "CMakeFiles/FOC_OpenLoop.elf.dir/UserApp/MT6701/MT6701.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/UserApp/MT6701/MT6701.c.obj.d"
+  "E:/Clion_for_STM32/FOC_OpenLoop/UserApp/retarget/retarget.c" "CMakeFiles/FOC_OpenLoop.elf.dir/UserApp/retarget/retarget.c.obj" "gcc" "CMakeFiles/FOC_OpenLoop.elf.dir/UserApp/retarget/retarget.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
